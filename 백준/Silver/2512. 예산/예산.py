19e914e903f77ae80ff218b5a70b693969ae03cc
Shin Seung Min <144583334+zjxlomin@users.noreply.github.com>
2025-05-10 12:45:50 +0900

    [Silver II] Title: 예산, Time: 44 ms, Memory: 35560 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -10,18 +10,18 @@ i=arr.count(max(arr))
 pdiff=[0]*(n-1)
 for j in range(n-1):
     pdiff[j]=arr[j]-arr[j+1]
-while s > m:
-    if i - 1 >= len(pdiff):
-        x = math.ceil((s - m) / i)
-        res -= x
+while(s>m):
+    if(i>=n):
+        x=math.ceil((s-m)/i)
+        res-=x
         break
-    p = pdiff[i - 1]
-    if s - p * i >= m:
-        s -= p * i
-        res -= p
+    p=pdiff[i-1]
+    if(s-p*i >= m):
+        s-=p*i
+        res-=p
     else:
-        x = math.ceil((s - m) / i)
-        res -= x
+        x=math.ceil((s-m)/i)
+        res-=x
         break
-    i += 1
+    i+=1
 print(res)
