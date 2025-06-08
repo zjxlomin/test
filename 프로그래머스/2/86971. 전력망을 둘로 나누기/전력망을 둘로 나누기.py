7c6a4cca37633926f9f1256b32653a133b4b0533
Shin Seung Min <144583334+zjxlomin@users.noreply.github.com>
2025-06-08 20:48:23 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.22 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,17 +1,18 @@
+from collections import deque
+
 def cut(a,b,adj,n):
     p=0
     visited=[False for _ in range(n)]
-    stack=[]
-    stack.append(a)
-    while(len(stack)!=0):
-        u=stack.pop(-1)
-        if not visited[u-1]: 
-            visited[u-1]=True
-            p+=1
+    deq=deque()
+    deq.append(a)
+    visited[a-1]=True
+    p+=1
+    while(len(deq)!=0):
+        u=deq.popleft()
         for v in adj[u]:
             if(v==b): continue
             if not visited[v-1]:
-                stack.append(v)
+                deq.append(v)
                 visited[v-1]=True
                 p+=1
     q=n-p
@@ -28,38 +29,11 @@ def solution(n, wires):
         except:
             adj[a]=[]
             adj[a].append(b)
-        linked[a]=len(adj[a])
         try: adj[b].append(a)
         except:
             adj[b]=[]
             adj[b].append(a)
-        linked[b]=len(adj[b])
-    """
-    linked=sorted(linked.items(),key=lambda x:x[1],reverse=True)
-    cand={1:[],2:[]}
-    fst=0
-    snd=0
-    for temp in linked:
-        if(temp[1]>=fst):
-            fst=temp[1]
-            cand[1].append(temp[0])
-            if(len(cand[1])==2): break
-        elif(temp[1]>=snd):
-            snd=temp[1]
-            cand[2].append(temp[0])
-        else: break
     
-    if(len(cand[1])==2):
-        a=cand[1][0]
-        b=cand[1][1]
-        answer=cut(a,b,adj,n)
-    else:
-        res=[]
-        a=cand[1][0]
-        for b in cand[2]:
-            res.append(cut(a,b,adj,n))
-        answer=min(res)
-    """
     res=[]
     for temp in wires:
         a=temp[0]
