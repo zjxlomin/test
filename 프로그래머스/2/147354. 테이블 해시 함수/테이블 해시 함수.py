cdee73922ec3f7d6dc4aec05e1359cabb1efdac2
Shin Seung Min <144583334+zjxlomin@users.noreply.github.com>
2025-06-26 17:02:10 +0900

    [level 2] Title: 테이블 해시 함수, Time: 84.18 ms, Memory: 63.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -0,0 +1,31 @@
+def solution(data, col, row_begin, row_end):
+    answer = 0
+    
+    col-=1
+    
+    temp={}
+    for i in range(len(data)):
+        k=data[i][col]
+        try: temp[k].append(data[i])
+        except:
+            temp[k]=[]
+            temp[k].append(data[i])
+        temp[k]=sorted(temp[k],key=lambda x:x[0], reverse=True)
+    temp=dict(sorted(temp.items(),key=lambda x:x[0]))
+    
+    new_data=[]
+    for a in temp.values():
+        new_data.extend(a)
+    
+    S_i=[]
+    for i in range(row_begin-1,row_end):
+        temp=new_data[i]
+        s=0
+        for x in temp:
+            s+=x%(i+1)
+        S_i.append(s)
+    
+    for x in S_i:
+        answer^=x
+    
+    return answer
