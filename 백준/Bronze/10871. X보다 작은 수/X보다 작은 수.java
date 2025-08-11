bc61eff253e1271dfbd076b87086491d4f4caa30
Shin Seung Min <144583334+zjxlomin@users.noreply.github.com>
2025-08-11 15:37:35 +0900

    [Bronze V] Title: X보다 작은 수, Time: 544 ms, Memory: 32088 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        int[] num = new int[n];
+
+        for(int i=0;i<n;i++){
+            num[i] = sc.nextInt();
+        }
+
+        for(int i=0;i<n;i++){
+            if(num[i]<x) {
+                System.out.print(num[i]);
+                System.out.print(' ');
+            }
+        }
+    }
+}
