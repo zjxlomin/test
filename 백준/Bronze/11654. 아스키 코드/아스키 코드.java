e1f42759ca6988577a5e07d9a1736f80a5dcca93
Shin Seung Min <144583334+zjxlomin@users.noreply.github.com>
2025-08-27 11:26:44 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.nextLine().charAt(0);
+
+        System.out.println((int)c);
+    }
+}
